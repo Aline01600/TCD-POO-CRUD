2dcc446dd05f17230688da185e421de18934b88c
Aline01600 <aline@DESKTOP-HGI29E5.localdomain>
2025-07-19 17:25:26 -0300

    Definição da Classe Aluno

--- Monitoria/src/main/java/entidade/Aluno.java
+++ Monitoria/src/main/java/entidade/Aluno.java
@@ -1,13 +1,67 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package entidade;
 
-/**
- *
- * @author aline
- */
 public class Aluno {
+    private String nome;
+    private String cpf;
+    private String disciplina;
+    private String matricula;
+    private String telefone;
+    private String email;
+    private boolean ehMonitor = false;
     
+    
+    public Aluno(String nome,String cpf, String disciplina, String matricula,String telefone, String email, boolean ehMonitor) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.disciplina = disciplina;
+        this.matricula = matricula;
+        this.telefone = telefone;
+        this.email = email;
+        this.ehMonitor = ehMonitor;
+    }
+    //<editor-fold defaultstate="collapsed" desc="Getters/Setters">
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getCpf() {
+        return cpf;
+    }
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+    public String getDisciplina() {
+        return disciplina;
+    }
+    public void setDisciplina(String disciplina) {
+        this.disciplina = disciplina;
+    }
+    public String getMatricula() {
+        return matricula;
+    }
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    } 
+    public String getTelefone() {
+        return telefone;
+    }
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public boolean getEhMonitor() {
+        return ehMonitor;
+    }
+     public void setEhMonitor(boolean ehMonitor) {
+        this.ehMonitor = ehMonitor;
+    }
+    //</editor-fold>
 }
