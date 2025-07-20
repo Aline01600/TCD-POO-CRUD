d30333a6f7ce4fac21332cc9ec7f402c6f44bdd9
Vitor Ribeiro <167051938+Vitor-Ribe@users.noreply.github.com>
2025-07-19 23:34:24 -0300

    Update Professor.java

--- src/entidade/Professor.java
+++ src/entidade/Professor.java
@@ -9,7 +9,6 @@ public class Professor {
     private int id;
     private String nome;
     private String departamento;
-    private boolean naLixeira = false;
 
     // Construtor de Professor
     public Professor(int id, String nome, String departamento) {
@@ -30,20 +29,12 @@ public class Professor {
         return departamento;
     }
 
-    public boolean isNaLixeira() {
-        return naLixeira;
-    }
-
     public void setNome(String nome) {
         this.nome = nome;
     }
 
     public void setDepartamento(String departamento) {
         this.departamento = departamento;
-    }
-
-    public void setNaLixeira(boolean naLixeira) {
-        this.naLixeira = naLixeira;
     }
 
     @Override
